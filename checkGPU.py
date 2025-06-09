b763424e9068da3baf06521c3c4fc21b6cfe79e0
LibbertDR <albertodironza84@gmail.com>
2025-06-09 23:56:43 +0200

    added GPU availability check script

--- checkGPU.py
+++ checkGPU.py
@@ -1,4 +1,10 @@
+import os
+os.add_dll_directory(r"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\bin")
 import tensorflow as tf
+import tensorflow as tf
+print(tf.__version__)
+print(tf.test.is_built_with_cuda())
+print(tf.config.list_physical_devices('GPU'))
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 print("GPUs:", tf.config.list_physical_devices('GPU'))
